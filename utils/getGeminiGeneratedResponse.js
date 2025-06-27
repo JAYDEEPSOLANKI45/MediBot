26b707525a18b8ee4c64d2f99f743d73152f1f2e
JAYDEEPSOLANKI45 <jaydeepsolanki4567@gmail.com>
2025-06-27 15:18:29 +0530

    error handling getGeminiGeneratedrResponse.js

--- utils/getGeminiGeneratedResponse.js
+++ utils/getGeminiGeneratedResponse.js
@@ -1,13 +1,20 @@
 // gemini
 const genAI = require('@google/generative-ai');
+const sendMessageToUser = require('./sendMessageToUser');
 const GeminiClient = new genAI.GoogleGenerativeAI(process.env.GEMINI_KEY);
 
 // function that will take prompt as an input and returns AI generated response
 async function getGeminiGeneratedResponse(prompt) {
-    const model = GeminiClient.getGenerativeModel({ model: process.env.GEMINI_MODEL_NAME }); 
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    return response.text()
+    try {
+        
+        const model = GeminiClient.getGenerativeModel({ model: process.env.GEMINI_MODEL_NAME }); 
+        const result = await model.generateContent(prompt);
+        const response = await result.response;
+        return response.text()
+    }
+    catch (err) {
+        return "Sorry the bot is unavailable right now. Please try again in a moment."
+    }
 }
 
 module.exports = getGeminiGeneratedResponse;
